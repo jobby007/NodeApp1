d6e17aa5ed96df30eb81cb84b7757f07c5fe2795
jobby007 <davidj@seoling.com>
2015-01-22 17:55:26 +0000

    s

--- routes/values.js
+++ routes/values.js
@@ -4,5 +4,27 @@ exports.randomStuff = function(req, res){
 };
 
 exports.findById = function(req, res) {
+	
+	var cs = "Driver={SQL Server Native Client 11.0};Server=tcp:i3optwqeq6.database.windows.net,1433;Database=SeolingListyWebApi_db;Trusted_Connection={Yes}";
+	
+	sql.open(cs, function(err, conn) {
+		if(err) {
+			res.send('ERROR :' + err);
+			return;
+		}
+		
+		conn.QueryRaw("SELECT * FROM List", function(err, results) {
+			if(err) {
+				res.send('ERROR :' + err);
+				return;
+			}
+			
+			res.send('RESULTS!! :' + results.rows.count);
+			return;
+			
+		});
+		
+	});
+	
 	res.send('you asked for this id :' + req.params.id);
 };
