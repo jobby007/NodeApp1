deb06996170e796ff4cd727691a69c86ee759f5f
jobby007 <davidj@seoling.com>
2015-01-22 17:31:18 +0000

    Go1!!

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes');
 var users = require('./routes/user');
+var values = require('./routes/values');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(app.router);
 
 app.get('/', routes.index);
 app.get('/users', users.list);
+app.get('/values', values.randomStuff);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
---
+++ routes/values.js
@@ -0,0 +1,4 @@
+/* GET users listing. */
+exports.randomStuff = function(req, res){
+  res.send('this is my stuff');
+};
